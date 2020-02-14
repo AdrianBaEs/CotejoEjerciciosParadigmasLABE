06d42a08516e55edf2891d6b2284340d1d3e1972
AdrianBaEs <eagleman977@gmail.com>
2020-02-13 22:45:02 -0600

    Paradigma Lógico - Prolog
    Paradigma Lógico - Prolog Listo

---
+++ Paradigma Logico (Prolog)/ParLogico.pl
@@ -0,0 +1,22 @@
+#ALUMNO: LUIS ADRIAN BALAM ESPADAS, DOCENTE: M.M.M.D. JOSE LEONEL PECH MAY
+#PARADIGMA LOGICO
+
+#Hechos y Reglas
+asignacion(init, init).
+asignacion(leonel, programacion).
+asignacion(antonio, redes).
+asignacion(jorge, ia).
+asignacion(marvin, hardware).
+asignacion(yesenia, graficacion).
+asignacion(francisca, gestion).
+
+horas(lunes, 7).
+horas(martes, 6).
+horas(miercoles, 5).
+horas(jueves, 6).
+horas(viernes, 5).
+
+esiniciosem(lunes).
+esmitadsem(miercoles).
+esfinsem(viernes).
+
